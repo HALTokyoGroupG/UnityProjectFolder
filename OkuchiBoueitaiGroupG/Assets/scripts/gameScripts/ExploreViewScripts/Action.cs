efd130f62a94088b3a9c96e87819f3e239c14fb1
渡邊エドモンズブラウンデイヴィッド明飛人 <watanabe.atori@gmail.com>
2016-05-27 17:10:25 +0900

    2016/5/27 渡邊エドモンズブラウンデイヴィッド明飛人 ゲームマネージャー、番野のNPC当たり判定と結合

---
+++ OkuchiBoueitaiGroupG/Assets/scripts/gameScripts/ExploreViewScripts/Action.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Action : MonoBehaviour {
+
+    //==============================
+    // ゲームの初期化処理
+    //==============================
+    void Start()
+    {
+
+    }
+
+    //==============================
+    // ゲームの更新処理
+    //==============================
+    void Update()
+    {
+        //　タップ処理
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject obj = hit.collider.gameObject;
+
+                if (obj.tag == "Action")
+                {
+                    //　ここに！をタップした場合の処理を追加する
+                    Debug.Log("選択中");
+                }
+            }
+        }
+    }
+}
