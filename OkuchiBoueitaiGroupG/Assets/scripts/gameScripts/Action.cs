b929dc37a1ed87961af84674a2f5a050af0cc54e
keitabanno <keita.banno@gmail.com>
2016-06-03 14:43:09 +0900

    微修正

--- OkuchiBoueitaiGroupG/Assets/scripts/gameScripts/Action.cs
+++ OkuchiBoueitaiGroupG/Assets/scripts/gameScripts/Action.cs
@@ -9,5 +9,24 @@ public class Action : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+
+        //　タップ処理
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject obj = hit.collider.gameObject;
+
+                if (obj.tag == "Action")
+                {
+                    //　ここに！をタップした場合の処理を追加する
+                    Debug.Log("選択中");
+                }
+            }
+        }
+
     }
 }
--- OkuchiBoueitaiGroupG/Assets/scripts/gameScripts/HitObject.cs
+++ OkuchiBoueitaiGroupG/Assets/scripts/gameScripts/HitObject.cs
@@ -29,23 +29,6 @@ public class HitObject : MonoBehaviour {
             return;
         }
 
-       //　タップ処理
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit = new RaycastHit();
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                GameObject obj = hit.collider.gameObject;
-
-                if (obj.tag == "Action")
-                {
-                    //　ここに！をタップした場合の処理を追加する
-                    Debug.Log("選択中");
-                }
-            }
-        }
     }
 
     //　当たった場合
